f7050f4edf7cb2f7414799091068c3fa19052d3e
psywah <psywah@gmail.com>
2017-08-07 16:14:10 -0600

    update

--- bin/parallel_job.sh
+++ bin/parallel_job.sh
@@ -1,13 +1,22 @@
-#SBATCH --job-name mpi_test
-#SBATCH --qos janus
+#!/bin/bash
+
+#SBATCH --job-name IN-standard
+#SBATCH --qos normal
 #SBATCH --nodes 2
-#SBATCH --ntasks-per-node 12
-#SBATCH --time 00:10:00
-#SBATCH --output mpi_test.out
+#SBATCH --ntasks-per-node 24
+#SBATCH --time 2:00:00
+#SBATCH --output IN-standard.out
+#SBATCH --partition shas
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=psywah@gmail.com
+
+## SBATCH --ntasks 48
 
 # the slurm module provides the srun command
-module load slurm
 
-module load intel/impi-13.0.0
+cd /scratch/summit/shgo7817/local-test/IN-standard/unitest/test_summit/bin
+source my-config
 
-srun mpi_test
+#mpiexec -n 4 ./CarotidIso.ex
+mpirun -n 48 ./CarotidIso.ex
+#echo Running on $(hostname --fqdn):  'Hello, world!'
